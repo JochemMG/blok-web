7d6a1cc92a77264771940f59801b0a47d484cd3e
JochemMG <90256187+JochemMG@users.noreply.github.com>
2021-09-30 12:26:46 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+var deButton = document.querySelector(".nav_button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+  console.log('hallo');
+}
